2cef269d7ae446bb3dfa0edbb66541524d6b3161
Mohanavenkatesh <mohanavenkatesh.a@gmail.com>
2025-08-07 09:36:16 +0530

    Day-2

---
+++ day-2/script.js
@@ -0,0 +1,48 @@
+// operators 
+
+let a = 5;
+let b = 5;
+
+// arithmatic operators
+
+console.log(a + b) // addition
+console.log(a - b) // subtraction
+console.log(a * b) // multiplication
+console.log(a / b) // division
+console.log(a % b) // modulus
+console.log(a ** b) // exponentiation
+console.log(a++); // increment
+console.log(a--); // decrement
+
+// assingment operators
+
+x = 5
+console.log("This is assignment operatores")
+console.log(x)
+console.log(x += 10)
+console.log(x -= 5)
+console.log(x += 25)
+console.log(x **= 2)
+console.log(x /= 3)
+
+// comparision operatior
+
+y = 1
+
+console.log(`This is comparission operators output :${y = 10}`)
+console.log(`This is comparission operators output :${y == '10'}`)
+console.log(`This is comparission operators output :${y === '10'}`)
+console.log(`This is comparission operators output :${y != '10'}`)
+console.log(`This is comparission operators output :${y !== '10'}`)
+console.log(`This is comparission operators output :${y < 20}`)
+console.log(`This is comparission operators output :${y > 20}`)
+console.log(`This is comparission operators output :${y >= 20}`)
+console.log(`This is comparission operators output :${y <= 20}`)
+
+// logical operators
+
+x = 2;
+
+console.log(`This is logical operator & output is : ${x}`)
+console.log(`This is logical operator & output is : ${x != y}`)
+
